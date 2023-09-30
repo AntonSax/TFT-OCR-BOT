d01072eed4c0f59d57ba57ca0f6a754c3f2e8f48
tony_sax <tony@wantenna.com>
2023-09-30 01:11:52 -0600

    Comps - Remove Kayle as a temp unit from the Roll the Rogues comp because she is useless. Add a Pandora's Items that doesn't use roman numerals.

--- set_9_5/comps/roll_the_rogues_ekko_qiyana_katarina.py
+++ set_9_5/comps/roll_the_rogues_ekko_qiyana_katarina.py
@@ -193,24 +193,14 @@ COMP = {
         "level": 2,
         "max_amount_of_items": 3,
         "final_comp": False
-    },
-    "Kayle": {
-        "board_position": 4,
-        "best_in_slot": ["InfinityEdge"],
-        "secondary_items": [],
-        "support_items_to_accept": [],
-        "trait_items_to_accept": ["SlayerEmblem"],
-        "ornn_items_to_accept": [],
-        "zaun_items_to_accept": [],
-        "level": 2,
-        "max_amount_of_items": 3,
-        "final_comp": False
     }
 }
 
 # No logic for certain augments meaning the bot won't know what to do if they are included in here
 # (Anything that changes gameplay or adds something to the bench).
 PRIMARY_AUGMENTS: list[str] = [
+    "Pandora's Items",  # since sometimes it doesn't pick up the roman numerals
+
     "Cybernetic Leech I",
     "Pandora's Items I",
     "Rogue Heart",
